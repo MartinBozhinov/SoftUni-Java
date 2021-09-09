6fdbbffd405879cf6bfd299d6cf6882475ae3fee
MartinBozhinov <sharkytrapera@gmail.com>
2021-09-09 21:21:05 +0300

    ExtractIntegersSolution

---
+++ Java Advanced/StreamsFilesAndDirectoriesLab/ExtractIntegers.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ExtractIntegers {
+    public static void main(String[] args) {
+
+        String userDir = System.getProperty("user.dir");
+
+        String pathIn = userDir + "/res/input.txt";
+        String pathOut = userDir + "/res/output.txt";
+
+        try (Scanner scanner = new Scanner(new FileReader(pathIn));
+             PrintWriter writer = new PrintWriter(pathOut)){
+            while (scanner.hasNext()){
+                scanner.next();
+            if (scanner.hasNextInt()) {
+                writer.println(scanner.nextInt());
+
+            }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
