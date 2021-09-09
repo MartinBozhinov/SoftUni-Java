5b9ebb953b01f7061e472111e7cbea85ea3eb9c9
MartinBozhinov <sharkytrapera@gmail.com>
2021-09-09 21:18:55 +0300

    CopyBytesSolution

---
+++ Java Advanced/StreamsFilesAndDirectoriesLab/CopyBytes.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CopyBytes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String userDir = System.getProperty("user.dir");
+
+        String pathIn = userDir + "/res/input.txt";
+        String pathOut = userDir + "/res/output.txt";
+
+          try  (FileInputStream fis = new FileInputStream(pathIn);
+                FileOutputStream fos = new FileOutputStream(pathOut)){
+              int oneByte = fis.read();
+               while (oneByte >= 0){
+                       if (oneByte == 32 || oneByte == 10){
+                        fos.write(oneByte);
+                       }else{
+
+                       String byteAsText = String.valueOf(oneByte);
+                   for (int i = 0; i < byteAsText.length(); i++) {
+                       int symbol = byteAsText.charAt(i);
+                       fos.write(symbol);
+                   }
+                       }
+                    oneByte = fis.read();
+               }
+
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+    }
+}
